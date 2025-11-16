36bbd725c4a297a24e17e55e6e40b3c6f15fc9c2
faatima30 <faatimaapdirahmaanali@gmail.com>
2025-11-16 11:08:02 +0300

    exercise 4 submitted

---
+++ exercise 4/index.js
@@ -0,0 +1,21 @@
+// 1. basic function 
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(5, 7));  
+console.log(add(1, 2)); 
+console.log(add(5, 79));  
+console.log(add(180, 40));  
+console.log(add(810, 230)); 
+
+// 2. basic function 
+const addExpression = function(a, b) {
+    return a + b;
+};
+
+console.log(add(5, 7));  
+console.log(add(1, 2)); 
+console.log(add(5, 79));  
+console.log(add(180, 40));  
+console.log(add(810, 230)); 
