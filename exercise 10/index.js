eb517de7fb5d44148371d3bc799c7f50b6a7a725
faatima30 <faatimaapdirahmaanali@gmail.com>
2025-11-16 16:23:24 +0300

    exercise 10 submitted

---
+++ exercise 10/index.js
@@ -0,0 +1,11 @@
+const library = [
+  { title: "Atomic Habits", author: "James Clear", year: 2018 },
+  { title: "Deep Work", author: "Cal Newport", year: 2016 },
+  { title: "The 7 Habits of Highly Effective People", author: "Stephen R. Covey", year: 1989 },
+  { title: "Getting Things Done", author: "David Allen", year: 2001 },
+  { title: "Essentialism", author: "Greg McKeown", year: 2014 }
+];
+
+console.log(library[0].title); 
+
+console.log(library[1].author); 
