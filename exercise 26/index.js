6d014b4433ec503b0215cd14b907023eb46e67bc
faatima30 <faatimaapdirahmaanali@gmail.com>
2025-11-18 17:22:09 +0300

    just formatted

--- exercise 26/index.js
+++ exercise 26/index.js
@@ -1,7 +1,7 @@
 // Blocking Example
 function blockingMessage() {
-    alert("Blocking for 2 seconds...");
-    return "Blocking done";
+  alert("Blocking for 2 seconds...");
+  return "Blocking done";
 }
 
 console.log("Start blocking example");
@@ -11,9 +11,9 @@ console.log("End blocking example");
 
 // Non-Blocking Example
 function nonBlockingMessage(callback) {
-    setTimeout(() => callback("Non-blocking done"), 2000);
+  setTimeout(() => callback("Non-blocking done"), 2000);
 }
 
 console.log("Start non-blocking example");
-nonBlockingMessage(msg => console.log(msg));
+nonBlockingMessage((msg) => console.log(msg));
 console.log("End non-blocking example");
